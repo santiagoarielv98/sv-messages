f8255b01c30f6dc2dbf6a074aafc8c8e691a28fb
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-16 16:43:54 -0300

    refactor: replace MessageService with use cases for message handling in chat component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MessageService } from './application/services/message.service';
-import { InMemoryMessageRepository } from './infrastructure/repositories/in-memory-message.repository';
+import { GetMessagesUseCase } from './application/use-cases/get-messages.use-case';
 import { MessageRepository } from './domain/repositories/message.repository';
-import { GetMessagesUseCase } from './domain/use-cases/get-messages.use-case';
-import { SendMessageUseCase } from './domain/use-cases/send-message.use-case';
-
+import { InMemoryMessageRepository } from './infrastructure/repositories/in-memory-message.repository';
+import { SendMessageUseCase } from './application/use-cases/send-message.use-case';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [
-    MessageService,
     {
       provide: GetMessagesUseCase,
       useFactory: (messageRepository: MessageRepository) =>
--- src/app/presentation/chat/chat.component.ts
+++ src/app/presentation/chat/chat.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { MessageService } from '../../application/services/message.service';
 import { Message } from '../../domain/entities/message';
 import { MessageListComponent } from '../components/message-list/message-list.component';
 import { MessageInputComponent } from '../components/message-input/message-input.component';
+import { GetMessagesUseCase } from '../../application/use-cases/get-messages.use-case';
+import { SendMessageUseCase } from '../../application/use-cases/send-message.use-case';
 
 @Component({
   selector: 'app-chat',
@@ -12,15 +13,17 @@ import { MessageInputComponent } from '../components/message-input/message-input
 })
 export class ChatComponent implements OnInit {
   messages: Message[] = [];
-
-  constructor(private messageService: MessageService) {}
+  constructor(
+    private getMessagesUseCase: GetMessagesUseCase,
+    private sendMessageUseCase: SendMessageUseCase,
+  ) {}
 
   async ngOnInit() {
-    this.messages = await this.messageService.getMessages();
+    this.messages = await this.getMessagesUseCase.execute();
   }
 
   async sendMessage(content: string) {
-    await this.messageService.sendMessage(content, 'user1');
-    this.messages = await this.messageService.getMessages();
+    await this.sendMessageUseCase.execute(content);
+    this.messages = await this.getMessagesUseCase.execute();
   }
 }
