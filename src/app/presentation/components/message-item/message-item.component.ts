36e70bb3b12690925c76c963b57f2c624b76437e
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-17 15:34:24 -0300

    feat: enhance message item component with responsive layout and styling

--- src/app/presentation/components/message-item/message-item.component.ts
+++ src/app/presentation/components/message-item/message-item.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Message } from '../../../domain/entities/message';
 import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-message-item',
-  imports: [MatCardModule],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './message-item.component.html',
   styleUrl: './message-item.component.scss',
 })
