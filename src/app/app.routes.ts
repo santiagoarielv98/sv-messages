eac56ca6d09994faba8ed812b535f666cf39369a
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-17 13:02:53 -0300

    feat: add sidenav component and main layout for improved navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,16 @@
 import { Routes } from '@angular/router';
 import { ChatComponent } from './presentation/chat/chat.component';
+import { MainLayoutComponent } from './presentation/layouts/main/main.layout';
 
-export const routes: Routes = [{ path: '', component: ChatComponent }];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: ChatComponent,
+      },
+    ],
+  },
+];
