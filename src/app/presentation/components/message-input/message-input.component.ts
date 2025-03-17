49e3931d273071e26fe66fa1e76cc9e141c4bf05
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-17 13:59:51 -0300

    feat: enhance message input component with Angular Material for improved UI

--- src/app/presentation/components/message-input/message-input.component.ts
+++ src/app/presentation/components/message-input/message-input.component.ts
@@ -1,9 +1,20 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-message-input',
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './message-input.component.html',
   styleUrl: './message-input.component.scss',
 })
