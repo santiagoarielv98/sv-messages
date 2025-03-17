93247cd4c56c79dfdcf23c4e1c2a6b2109a43c3b
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-17 10:29:08 -0300

    feat: add GetCurrentUserUseCase provider to message list component

--- src/app/presentation/components/message-list/message-list.component.ts
+++ src/app/presentation/components/message-list/message-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { GetCurrentUserUseCase } from '../../../application/use-cases/auth.use-case';
 import { Message } from '../../../domain/entities/message';
 import { MessageItemComponent } from '../message-item/message-item.component';
 
 @Component({
   selector: 'app-message-list',
   imports: [MessageItemComponent],
+  providers: [GetCurrentUserUseCase],
   templateUrl: './message-list.component.html',
   styleUrl: './message-list.component.scss',
 })
