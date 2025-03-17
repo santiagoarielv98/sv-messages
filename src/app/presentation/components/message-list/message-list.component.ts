978d64093a6526704a17f2bc97675a2096888922
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-17 16:05:27 -0300

    feat: implement auto-scrolling for message list on new messages

--- src/app/presentation/components/message-list/message-list.component.ts
+++ src/app/presentation/components/message-list/message-list.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { GetCurrentUserUseCase } from '../../../application/use-cases/auth.use-case';
 import { Message } from '../../../domain/entities/message';
 import { MessageItemComponent } from '../message-item/message-item.component';
@@ -12,12 +20,32 @@ import { AsyncPipe } from '@angular/common';
   templateUrl: './message-list.component.html',
   styleUrl: './message-list.component.scss',
 })
-export class MessageListComponent {
+export class MessageListComponent implements AfterViewInit, OnChanges {
   @Input() messages: Message[] | null = [];
+  @ViewChild('bottom')
+  private bottom!: ElementRef<HTMLDivElement>;
 
   currentUser$: Observable<User | null>;
 
   constructor(private getCurrentUserUseCase: GetCurrentUserUseCase) {
     this.currentUser$ = this.getCurrentUserUseCase.execute();
+  }
+
+  ngAfterViewInit() {
+    this.scrollToBottom();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['messages'] && this.bottom) {
+      setTimeout(() => this.scrollToBottom(), 0);
+    }
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.bottom.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    } catch (err) {
+      console.error('Error scrolling to bottom:', err);
+    }
   }
 }
