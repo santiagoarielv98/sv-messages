c4f53695aa579092b1036a6fb510667a115b76ea
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-19 15:49:48 -0300

    feat: update sidenav component to use dynamic title and improve responsiveness

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, viewChild } from '@angular/core';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Chat, ChatService } from '../../chat.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav',
@@ -25,11 +27,26 @@ import { Chat, ChatService } from '../../chat.service';
   ],
 })
 export class SidenavComponent {
+  title = 'SVChat';
   private chatService = inject(ChatService);
   selectedChat = this.chatService.selectedChat;
   chats = this.chatService.chats;
+  drawer = viewChild.required<MatSidenav>('drawer');
+
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay(),
+    );
 
   selectChat(chat: Chat) {
     this.chatService.selectChat(chat);
+
+    if (this.drawer().mode === 'over') {
+      this.drawer().close();
+    }
   }
 }
