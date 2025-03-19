1a96179ad8f7684199b10583a863e5d88c4d8eb3
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-19 14:09:27 -0300

    refactor: simplify sidenav component by removing unused imports and template code

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,79 +1,13 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialog } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
-import { ChatService } from '../../chat.service';
-import { CreateChatDialogComponent } from '../create-chat-dialog/create-chat-dialog.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatIconModule,
-    AsyncPipe,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-  ],
+  imports: [RouterOutlet],
 })
 export class SidenavComponent {
   title = 'SV / Messages';
-  private breakpointObserver = inject(BreakpointObserver);
-  private chatService = inject(ChatService);
-  private router = inject(Router);
-
-  readonly dialog = inject(MatDialog);
-
-  chats$ = this.chatService.chats$;
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay(),
-    );
-
-  openDialog(): void {
-    this.dialog.open(CreateChatDialogComponent);
-  }
-
-  isRouteActive(chatId: string): boolean {
-    return this.router.isActive(`/chat/${chatId}`, {
-      paths: 'exact',
-      queryParams: 'exact',
-      fragment: 'ignored',
-      matrixParams: 'ignored',
-    });
-  }
-
-  isChatRouteActive(): boolean {
-    return this.router.isActive('/chat', {
-      paths: 'subset',
-      queryParams: 'exact',
-      fragment: 'ignored',
-      matrixParams: 'ignored',
-    });
-  }
-
-  goBack(): void {
-    this.router.navigate(['/']);
-  }
 }
