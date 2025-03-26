3dc7dc6b9d5b59fef3e7d0d374f20afbb12d3d37
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-26 00:50:23 -0300

    feat: update Firebase workflows for environment configuration and add user listing in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
+interface User {
+  id: string;
+  name: string;
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   template: `
     <h1>Welcome to {{ title }}!</h1>
-
+    <h2>Users</h2>
+    <ul>
+      @for (user of user$ | async; track user.id) {
+        {{ user.id }}
+      } @empty {
+        No users found.
+      }
+    </ul>
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
+  private firestore = inject(Firestore);
+  private userCollection = collection(this.firestore, 'users');
+  user$ = collectionData(this.userCollection, { idField: 'id' }) as Observable<
+    User[]
+  >;
+
   title = 'sv-lab2';
 }
