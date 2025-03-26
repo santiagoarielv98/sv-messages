f15f2925d7b578bf294b24c0142261716911c6ed
Santiago Ariel <santiagoarielv98@gmail.com>
2025-03-26 08:48:47 -0300

    refactor: simplify app component by removing unused imports and user listing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,10 @@
-import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable } from 'rxjs';
 
-interface User {
-  id: string;
-  name: string;
-}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AsyncPipe],
-  template: `
-    <h1>Welcome to {{ title }}!!!!!!</h1>
-    <h2>Users</h2>
-    <ul>
-      @for (user of user$ | async; track user.id) {
-        {{ user.id }}
-      } @empty {
-        No users found.
-      }
-    </ul>
-    <router-outlet />
-  `,
+  imports: [RouterOutlet],
+  template: `<router-outlet /> `,
   styles: [],
 })
-export class AppComponent {
-  private firestore = inject(Firestore);
-  private userCollection = collection(this.firestore, 'users');
-  user$ = collectionData(this.userCollection, { idField: 'id' }) as Observable<
-    User[]
-  >;
-
-  title = 'sv-lab2';
-}
+export class AppComponent {}
